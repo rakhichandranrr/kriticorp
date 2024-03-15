a9dc27c82f5e19472d3bb8ba1aef7f8c5b14065f
Rakhi <rakhichandranrr@gmail.com>
2024-03-15 13:39:42 +0530

    404 and search page changes

--- wp-content/themes/kriticorp/404.php
+++ wp-content/themes/kriticorp/404.php
@@ -1,27 +1,60 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package WordPress
- * @subpackage kriticorp
- * @since kriticorp 1.0
- */
-
-get_header();
-?>
-
-	<header class="page-header alignwide">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing here', 'kriticorp' ); ?></h1>
-	</header><!-- .page-header -->
-
-	<div class="error-404 not-found default-max-width">
-		<div class="page-content">
-			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'kriticorp' ); ?></p>
-			<?php get_search_form(); ?>
-		</div><!-- .page-content -->
-	</div><!-- .error-404 -->
-
-<?php
-get_footer();
+<?php
+
+/**
+
+ * The template for displaying 404 pages (not found)
+
+ *
+
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+
+ *
+
+ * @package WordPress
+
+ * @subpackage kriticorp
+
+ * @since kriticorp 1.0
+
+ */
+
+
+
+get_header();
+
+?>
+
+<div id="content" class="site-content "> 
+  
+  <!---about--->
+  
+  <section class="about-section"> 
+    
+    <!--===============spacing==============-->
+    
+    <div class="pd_top_90"></div>
+    
+    <!--===============spacing==============-->
+    
+    <div class="container">
+      <div class="row">
+        <p>
+          <?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'kriticorp' ); ?>
+        </p>
+        <?php get_search_form(); ?>
+      </div>
+    </div>
+    
+    <!--===============spacing==============-->
+    
+    <div class="pd_bottom_70"></div>
+    
+    <!--===============spacing==============--> 
+    
+  </section>
+</div>
+<?php
+
+get_footer();
+
+
